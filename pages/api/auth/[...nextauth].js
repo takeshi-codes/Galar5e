ec8e8e6365e499e6aee30b10437f197110dcdcbf
its-dgreen <dylan@ctrlaltdylan.com>
2021-02-03 14:41:04 -0600

    feat: added discord login

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -6,6 +6,10 @@ const options = {
         clientId: process.env.GOOGLE_ID,
         clientSecret: process.env.GOOGLE_SECRET,
     }),
+    Providers.Discord({
+      clientId: process.env.DISCORD_CLIENT_ID,
+      clientSecret: process.env.DISCORD_CLIENT_SECRET
+    })
   ],
   database: process.env.MONGODB_URI,
 }
